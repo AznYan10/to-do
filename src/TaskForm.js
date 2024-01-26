0eee82c3155588e193e919364383c1093d4b7725
yan.aung <aznyan5510@gmail.com>
2024-01-26 16:17:20 -0500

    Added files to start react and baseline for the project

---
+++ src/TaskForm.js
@@ -0,0 +1,10 @@
+export default function TaskForm() {
+  return (
+    <div>
+      <form>
+        <button>+</button>
+        <input type='text' placeholder="Your next task"/>
+      </form>
+    </div> 
+  );
+}
