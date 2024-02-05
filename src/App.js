1c84af2499e11c47e2170de4e70eb252212e90ca
yan.aung <aznyan5510@gmail.com>
2024-02-05 14:43:11 -0500

    Fixed TaskFrom.js to export properly and added a Task.js when new tasks are added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Task from './Task';
 import TaskForm from './TaskForm';
 
 function App() {
   return(
     <div>
       <TaskForm />
+      <Task />
     </div>
   );
 }
--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,10 +1,14 @@
+import {useState} from "react";
+
 export default function TaskForm() {
-  return (
-    <div>
-      <form>
-        <button>+</button>
-        <input type='text' placeholder="Your next task"/>
-      </form>
-    </div> 
-  );
+    const [taskName,setTaskName] = useState('');
+    return (
+        <form>
+            <button>+</button>
+            <input type='text' 
+            value={taskName}
+            onChange={ev => setTaskName(ev.target.value)}
+            placeholder="Your next task"/>
+        </form>
+    );
 }
